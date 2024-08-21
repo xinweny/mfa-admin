326cda29212d6fe720e3bc5bbc065966cbf6a04e
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-08-21 19:20:56 -0400

    remove non-existent references

--- api/Mfa/src/Modules/Address/AddressModule.cs
+++ api/Mfa/src/Modules/Address/AddressModule.cs
@@ -6,7 +6,6 @@ public static class AddressModule {
     public static void AddAddressModule(this IServiceCollection services) {
         services.AddScoped<IAddressRepository, AddressRepository>();
         services.AddScoped<IAddressService, AddressService>();
-        services.AddScoped<IMembershipAddressService, MembershipAddressService>();
         services.AddScoped<IValidator<AddressModel>, AddressValidator>();
     }
 }
--- api/Mfa/src/Modules/Membership/Repositories/IMembershipRepository.cs
+++ api/Mfa/src/Modules/Membership/Repositories/IMembershipRepository.cs
@@ -5,6 +5,5 @@ public interface IMembershipRepository {
     Task<MembershipModel> GetMembershipById(int id);
     Task CreateMembership(MembershipModel membership);
     Task UpdateMembership(MembershipModel membership, UpdateMembershipRequest req);
-    Task UpdateMembershipAddressId(MembershipModel membership, int? addressId);
     Task DeleteMembership(MembershipModel membership);
 }
