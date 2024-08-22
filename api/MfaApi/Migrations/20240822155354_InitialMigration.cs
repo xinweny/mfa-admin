64745b593bffe2af2e91e86accbcfb3305407439
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-08-22 11:58:13 -0400

    add initial migration

---
+++ api/MfaApi/Migrations/20240822155354_InitialMigration.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MfaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "addresses",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    line1 = table.Column<string>(type: "text", nullable: false),
+                    line2 = table.Column<string>(type: "text", nullable: true),
+                    line3 = table.Column<string>(type: "text", nullable: true),
+                    city = table.Column<string>(type: "text", nullable: false),
+                    postal_code = table.Column<string>(type: "text", nullable: false),
+                    province = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_addresses", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    user_name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "memberships",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    membership_type = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    address_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_memberships", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_memberships_addresses_address_id",
+                        column: x => x.address_id,
+                        principalTable: "addresses",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "members",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    first_name = table.Column<string>(type: "text", nullable: false),
+                    last_name = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    phone_number = table.Column<string>(type: "text", nullable: true),
+                    joined_date = table.Column<DateOnly>(type: "date", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    membership_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_members", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_members_memberships_membership_id",
+                        column: x => x.membership_id,
+                        principalTable: "memberships",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "membership_dues",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    year = table.Column<int>(type: "integer", nullable: false),
+                    payment_method = table.Column<int>(type: "integer", nullable: false),
+                    amount_paid = table.Column<int>(type: "integer", nullable: false),
+                    payment_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    membership_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_membership_dues", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_membership_dues_memberships_membership_id",
+                        column: x => x.membership_id,
+                        principalTable: "memberships",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "board_members",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    start_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    end_date = table.Column<DateOnly>(type: "date", nullable: true),
+                    board_position = table.Column<int>(type: "integer", nullable: false),
+                    member_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_board_members", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_board_members_members_member_id",
+                        column: x => x.member_id,
+                        principalTable: "members",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "cultural_exchanges",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    year = table.Column<int>(type: "integer", nullable: false),
+                    exchange_type = table.Column<int>(type: "integer", nullable: false),
+                    member_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cultural_exchanges", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_cultural_exchanges_members_member_id",
+                        column: x => x.member_id,
+                        principalTable: "members",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_board_members_member_id",
+                table: "board_members",
+                column: "member_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cultural_exchanges_member_id",
+                table: "cultural_exchanges",
+                column: "member_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_members_membership_id",
+                table: "members",
+                column: "membership_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_membership_dues_membership_id",
+                table: "membership_dues",
+                column: "membership_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_memberships_address_id",
+                table: "memberships",
+                column: "address_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "board_members");
+
+            migrationBuilder.DropTable(
+                name: "cultural_exchanges");
+
+            migrationBuilder.DropTable(
+                name: "membership_dues");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "members");
+
+            migrationBuilder.DropTable(
+                name: "memberships");
+
+            migrationBuilder.DropTable(
+                name: "addresses");
+        }
+    }
+}
