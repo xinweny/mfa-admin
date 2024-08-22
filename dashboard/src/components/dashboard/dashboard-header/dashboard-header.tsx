1647592688bc5014a68ffcbbf8809976a2eef033
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-08-22 19:50:54 -0400

    remove tailwind

--- dashboard/src/components/dashboard/dashboard-header/dashboard-header.tsx
+++ dashboard/src/components/dashboard/dashboard-header/dashboard-header.tsx
@@ -1,20 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { AppBar } from "@mui/material";
+
 import mfaLogo from "@/assets/images/mfa-logo.png";
 
 export function DashboardHeader() {
   return (
-    <header className="p-4 flex items-center justify-between shadow-lg bg-">
+    <AppBar>
       <Link href="/dashboard">
         <Image
           src={mfaLogo}
-          className="rounded-full"
           height={48}
           width={48}
           alt="Mississauga Friendship Association"
         />
       </Link>
-    </header>
+    </AppBar>
   );
 }
