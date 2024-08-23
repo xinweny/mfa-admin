8a3a0fda7e9445233e6ac2a10f4f28d0865363fe
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-08-23 01:11:14 -0400

    restore tailwindcss

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import '@/styles/globals.css';
+import '@/styles/reset.css';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
