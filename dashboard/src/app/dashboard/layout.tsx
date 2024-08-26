e2f11229ccccbc2431a19b469d80880722c565b3
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-08-26 15:57:22 -0400

    change to dashboard component folder

--- dashboard/src/app/dashboard/layout.tsx
+++ dashboard/src/app/dashboard/layout.tsx
@@ -1,3 +1,3 @@
-import { DashboardLayout } from '@/components/layout/dashboard-layout';
+import { DashboardLayout } from '@/components/dashboard/dashboard-layout';
 
 export default DashboardLayout;
