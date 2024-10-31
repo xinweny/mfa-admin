274404fbaac1f45d3758e698760b646b95af1974
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-10-31 17:44:14 -0400

    switch to helper fetch for getting access token

--- dashboard/src/core/api/utils/mfa-api-fetch.ts
+++ dashboard/src/core/api/utils/mfa-api-fetch.ts
@@ -1,16 +1,21 @@
+import { getAccessToken } from '@/modules/auth/utils';
+
 interface MfaApiRequestInit extends RequestInit {
   body: any;
 }
 
 export const mfaApiFetch = async (path: string, req?: MfaApiRequestInit) => {
+  const accessToken = await getAccessToken();
+
   return await fetch(
     `${process.env.NEXT_PUBLIC_MFA_API_URL}/${path}`,
     req
       ? {
       method: req.method,
-      body: JSON.stringify(req.body),
+      body: req.body ? JSON.stringify(req.body) : undefined,
       headers: {
         'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
         ...req.headers,
       },
     }
