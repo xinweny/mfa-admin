edfd0e121c55c797e525870a159729022f9ea8f8
Xin Wen Yap <yapxinwen1608@gmail.com>
2025-02-17 00:53:42 -0500

    fix address formatting

--- dashboard/src/modules/address/utils/format-address-string.ts
+++ dashboard/src/modules/address/utils/format-address-string.ts
@@ -1,5 +1,5 @@
 import { Address, provinceLabels } from '../types';
 
 export const formatAddressString = (address: Address) => {
-  return `${address.line1}, ${address.line2 ? `${address.line2}, ` : ''}, ${address.city}, ${provinceLabels[address.province]} ${address.postalCode}`
+  return `${address.line1}, ${address.line2 ? `${address.line2}, ` : ''}${address.city}, ${provinceLabels[address.province]} ${address.postalCode}`
 }
