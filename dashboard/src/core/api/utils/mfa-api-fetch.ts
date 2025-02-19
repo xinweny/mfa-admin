0ea5642afafb93dd7b25a70d786242e7c6265c60
Xin Wen Yap <yapxinwen1608@gmail.com>
2025-02-19 12:05:30 -0500

    fix auth header specification for get requests

--- dashboard/src/core/api/utils/mfa-api-fetch.ts
+++ dashboard/src/core/api/utils/mfa-api-fetch.ts
@@ -4,13 +4,17 @@ interface MfaApiRequestInit extends RequestInit {
   body: any;
 }
 
-export const mfaApiFetch = async (path: string, req?: MfaApiRequestInit) => {
+export const mfaApiFetch = async (
+  path: string,
+  req: MfaApiRequestInit = {
+    method: 'get',
+    body: undefined,
+  }) => {
   const accessToken = await getAccessToken();
 
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_MFA_API_URL}/${path}`,
-    req
-      ? {
+    {
       method: req.method,
       body: req.body ? JSON.stringify(req.body) : undefined,
       headers: {
@@ -19,7 +23,6 @@ export const mfaApiFetch = async (path: string, req?: MfaApiRequestInit) => {
         ...req?.headers,
       },
     }
-      : undefined
   );
 
   return res;
